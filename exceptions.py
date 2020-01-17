6bdd141fee6114ca859ee0ac8bd8393904d87be4
Dmitriy Shmatko <dmitriy.shmatko@gameloft.com>
2020-01-18 01:51:22 +0200

    + test stage

---
+++ exceptions.py
@@ -0,0 +1,9 @@
+class GameOver(Exception):
+    def __init__(self, objects):
+        super().__init__()
+        self.player_name = str(objects.player_name)
+        self.player_score = str(objects.score)
+
+
+class EnemyDown(Exception):
+    pass
